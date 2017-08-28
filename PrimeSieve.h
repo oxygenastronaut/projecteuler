092fcfccd0b941b23409a028347bf24abc2612ba
Jonathan Shipley <jship1901@gmail.com>
2017-08-28 15:29:42 -0500

    Add files via upload

---
+++ PrimeSieve.h
@@ -0,0 +1,23 @@
+// prime sieve object class
+
+#ifndef PrimeSieve_H
+#define PrimeSieve_H
+
+class PrimeSieve
+{	
+	public:
+		int upperlimit;
+
+	int PrimeSieve::generatePrimes();
+
+
+
+
+
+
+
+
+}
+
+
+#endif
